6b925b4fffdc92b7b43b6ac85c5ebd0f8b300d3d
Firsov Pavel Dmitrievich <p.d.firsov@gmail.com>
2024-09-13 00:19:41 +0300

    Update life communication

--- panels/life_communication.py
+++ panels/life_communication.py
@@ -7,85 +7,102 @@ import flet as ft
 class LifeCommunicationPanel(ft.ResponsiveRow):
     def __init__(self, od):
         super().__init__()
-        self.te_prod_hb = ft.TextField(label="Producer Heartbeat Time, ms:",
-                                       width=200,
-                                       value=od.object_dictionary.get_variable(0x1017, 0).default)
         self.visible = False
+        self.expand = True
 
-        def handle_confirm_dismiss(e: ft.DismissibleDismissEvent):
-            if e.direction == ft.DismissDirection.END_TO_START:  # right-to-left slide
-                # save current dismissible to dialog's data, for confirmation in handle_dlg_action_clicked
-                if len(self.list_view.controls) == 1:
-                    self.list_view.controls.append(
-                        ft.TextButton("Add Consumer Heartbeat Time", data=True, on_click=start_chb))
-                e.control.confirm_dismiss(True)
-            else:  # left-to-right slide
-                for item_diss in range(len(self.list_view.controls)):
-                    if e.control.uid == self.list_view.controls[item_diss].uid:
-                        self.list_view.controls.insert(item_diss + 1, add_consumer_heartbeat(f'{hex(0x01)}',
-                                                                                             1000))
-                        break
+        self.te_prod_hb = ft.TextField(label="Time, ms:",
+                                       col=4,
+                                       value=od.object_dictionary.get_variable(0x1017, 0).default)
+        self.lv_consumer_hb = ft.ListView(expand=1, spacing=10, padding=20, height=400, col=8)
+        self.t_consumer_hb = ft.Text("Consumer Heartbeat")
 
-                e.control.confirm_dismiss(False)
-                self.update()
+        def start_chb(e):
+            self.lv_consumer_hb.controls.clear()
+            self.lv_consumer_hb.controls.append(add_consumer_heartbeat(f'{hex(0x01)}', f'{1000}'))
+            self.t_consumer_hb.value = "Consumer Heartbeat :" + str(len(self.lv_consumer_hb.controls))
+            self.update()
 
-        def handle_dismiss(e):
-            print('on_dismiss')
-            e.control.parent.controls.remove(e.control)
+        self.__btn_first_chb = ft.TextButton("Add Consumer Heartbeat Time", data=True, col=8, on_click=start_chb)
+
+        def button_add(e):
+            for item_diss in range(len(self.lv_consumer_hb.controls)):
+                objs = self.lv_consumer_hb.controls
+                if e.control.parent.parent.uid == objs[item_diss].uid:
+                    id = int(objs[item_diss].content.controls[0].value, 16) + 1
+                    objs.insert(item_diss + 1, add_consumer_heartbeat(f'{hex(id)}', 1000))
+                self.t_consumer_hb.value = "Consumer Heartbeat :" + str(len(self.lv_consumer_hb.controls))
+            self.update()
+
+        def button_delete(e):
+            item_delete = 0
+            for item_diss in range(len(self.lv_consumer_hb.controls)):
+                if e.control.parent.parent.uid == self.lv_consumer_hb.controls[item_diss].uid:
+                    item_delete = item_diss
+            self.lv_consumer_hb.controls.remove(self.lv_consumer_hb.controls[item_delete])
+            self.t_consumer_hb.value = "Consumer Heartbeat :" + str(len(self.lv_consumer_hb.controls))
+            if len(self.lv_consumer_hb.controls) == 0:
+                self.lv_consumer_hb.controls.append(self.__btn_first_chb)
             self.update()
 
         def add_consumer_heartbeat(node_id, time):
-
-            return ft.Dismissible(
-                content=ft.Row([
+            return ft.Container(
+                content=ft.ResponsiveRow([
                     ft.TextField(
-                        width=200,
                         label="Node ID",
+                        col=5,
                         value=node_id),
                     ft.TextField(
-                        width=200,
                         label="Time, ms",
+                        col=5,
                         value=time),
+                    ft.IconButton(
+                        icon=ft.icons.ADD,
+                        icon_color="blue400",
+                        tooltip="Add",
+                        on_click=button_add,
+                        col=1
+                    ),
+                    ft.IconButton(
+                        icon=ft.icons.DELETE,
+                        icon_color="pink600",
+                        tooltip="Delete",
+                        on_click=button_delete,
+                        col=1
+                    ),
                 ], ),
-                dismiss_direction=ft.DismissDirection.HORIZONTAL,
-                background=ft.Container(ft.Icon(ft.icons.ADD), bgcolor=ft.colors.GREEN,
-                                        alignment=ft.alignment.center_left, padding=ft.padding.only(left=10)),
-                secondary_background=ft.Container(ft.Icon(ft.icons.DELETE), bgcolor=ft.colors.RED,
-                                                  alignment=ft.alignment.center_right,
-                                                  padding=ft.padding.only(right=10)),
-                on_dismiss=handle_dismiss,
-                on_confirm_dismiss=handle_confirm_dismiss,
-                dismiss_thresholds={
-                    ft.DismissDirection.END_TO_START: 0.2,
-                    ft.DismissDirection.START_TO_END: 0.2,
-                },
             )
-
-        self.list_view = ft.ListView(expand=1, spacing=10, padding=20, height=300, width=500)
-
-        def start_chb(e):
-            self.list_view.controls.clear()
-            self.list_view.controls.append(add_consumer_heartbeat(f'{hex(0x01)}',
-                                                                  f'{1000}'))
-            self.update()
 
         if od.object_dictionary.get_variable(0x1016, 0) is not None:
             for item in range(od.object_dictionary.get_variable(0x1016, 0).default):
                 data = od.object_dictionary.get_variable(0x1016, item + 1).default
-                self.list_view.controls.append(add_consumer_heartbeat(f'{hex((data >> 16) & 0xFF)}',
-                                                                      f'{(data & 0xFFFF)}')
-                                               )
+                self.lv_consumer_hb.controls.append(add_consumer_heartbeat(f'{hex((data >> 16) & 0xFF)}',
+                                                                           f'{(data & 0xFFFF)}')
+                                                    )
+            self.t_consumer_hb.value = "Consumer Heartbeat :" + str(len(self.lv_consumer_hb.controls))
         else:
-            self.list_view.controls.append(
-                ft.TextButton("Add Consumer Heartbeat Time", data=True, on_click=start_chb))
+            self.lv_consumer_hb.controls.append(self.__btn_first_chb)
 
-        self.controls = [ft.Row([
-            ft.Column([
-                ft.Text("Consumer Heartbeat Time, ms"),
-                self.list_view,
-            ]),
-            self.te_prod_hb,
-        ])]
+        self.controls = [
+            ft.ResponsiveRow(
+                [
+                    ft.Container(
+                        ft.Text("Producer Heartbeat"),
+                        col=4,
+                    ),
+                    ft.Container(
+                        self.t_consumer_hb,
+                        col=8,
+                    ),
+                ],
+            ),
+
+            ft.ResponsiveRow(
+                [
+                    self.te_prod_hb,
+                    self.lv_consumer_hb,
+                ],
+            ),
+        ]
 
     def update_od(self, od):
         od.object_dictionary.get_variable(0x1017).default = self.te_prod_hb.value
@@ -100,14 +117,14 @@ class LifeCommunicationPanel(ft.ResponsiveRow):
         var = canopen.objectdictionary.ODVariable("Number of Entries", 0x1016, 0)
         var.access_type = "ro"
         var.data_type = canopen.objectdictionary.datatypes.UNSIGNED8
-        var.default = len(self.list_view.controls)
+        var.default = len(self.lv_consumer_hb.controls)
         od_cd.add_member(var)
-        for item in range(len(self.list_view.controls)):
+        for item in range(len(self.lv_consumer_hb.controls)):
             var = canopen.objectdictionary.ODVariable("Consumer Heartbeat Time", 0x1016, item + 1)
             var.access_type = "rw"
             var.data_type = canopen.objectdictionary.datatypes.UNSIGNED32
-            var.default = int(self.list_view.controls[item].content.controls[0].value, 16) << 16 | int(
-                self.list_view.controls[item].content.controls[1].value)
+            var.default = int(self.lv_consumer_hb.controls[item].content.controls[0].value, 16) << 16 | int(
+                self.lv_consumer_hb.controls[item].content.controls[1].value)
             od_cd.add_member(var)
 
         od.object_dictionary.add_object(od_cd)
--- panels/sdo_communication.py
+++ panels/sdo_communication.py
@@ -9,27 +9,29 @@ class SdoCommunicationPanel(ft.ResponsiveRow):
         super().__init__()
         self.visible = False
         self.expand = True
-        self.od = od
 
-        self.sdo_server_tx = ft.TextField(label="COB ID Client to Server", value="$NODEID+0x600", col=2)
-        self.sdo_server_rx = ft.TextField(label="COB ID Server to Client", value="$NODEID+0x580", col=2)
+        self.sdo_server_tx = ft.TextField(label="COB ID Rx SDO", value="0x600", col=2)
+        self.sdo_server_rx = ft.TextField(label="COB ID Tx SDO", value="0x580", col=2)
 
         self.lv_sdo_client = ft.ListView(expand=1, spacing=10, padding=20, height=400, col=8)
+        self.t_sdo_client = ft.Text("SDO Client")
 
-        def start_chb(e):
+        def button_first_sdo_cli(e):
             self.lv_sdo_client.controls.clear()
-            self.lv_sdo_client.controls.append(add_consumer_heartbeat(f'{hex(0x01)}',
-                                                                      f'{1000}'))
+            self.lv_sdo_client.controls.append(add_consumer_heartbeat(f'{hex(0x01)}', f'{hex(0x01)}', f'{hex(0x01)}'))
+            self.t_sdo_client.value = "SDO Client :" + str(len(self.lv_sdo_client.controls))
             self.update()
 
-        self.__add_btn = ft.TextButton("Add Consumer Heartbeat Time", data=True, col=8, on_click=start_chb)
-        self.lv_sdo_client.controls.append(self.__add_btn)
+        self.__add_btn = ft.TextButton("Add Consumer Heartbeat Time", data=True, col=8, on_click=button_first_sdo_cli)
 
         def button_add(e):
             for item_diss in range(len(self.lv_sdo_client.controls)):
-                if e.control.parent.parent.uid == self.lv_sdo_client.controls[item_diss].uid:
-                    self.lv_sdo_client.controls.insert(item_diss + 1, add_consumer_heartbeat(f'{hex(0x01)}',
-                                                                                             1000))
+                objs = self.lv_sdo_client.controls
+                if e.control.parent.parent.uid == objs[item_diss].uid:
+                    id = int(objs[item_diss].content.controls[0].value, 16) + 1
+                    objs.insert(item_diss + 1,
+                                add_consumer_heartbeat(f'{hex(id)}', f'{hex(id + 0x600)}', f'{hex(id + 0x580)}'))
+            self.t_sdo_client.value = "SDO Client :" + str(len(self.lv_sdo_client.controls))
             self.update()
 
         def button_delete(e):
@@ -38,25 +40,26 @@ class SdoCommunicationPanel(ft.ResponsiveRow):
                 if e.control.parent.parent.uid == self.lv_sdo_client.controls[item_diss].uid:
                     item_delete = item_diss
             self.lv_sdo_client.controls.remove(self.lv_sdo_client.controls[item_delete])
+            self.t_sdo_client.value = "SDO Client :" + str(len(self.lv_sdo_client.controls))
             if len(self.lv_sdo_client.controls) == 0:
                 self.lv_sdo_client.controls.append(self.__add_btn)
             self.update()
 
-        def add_consumer_heartbeat(node_id, time):
+        def add_consumer_heartbeat(node_id, tx_cobid, rx_cobid):
             return ft.Container(
                 content=ft.ResponsiveRow([
                     ft.TextField(
-                        label="COB ID Client to Server",
+                        label="Node ID",
                         col=2,
                         value=node_id),
                     ft.TextField(
-                        label="COB ID Server to Client",
+                        label="COB ID Tx SDO",
                         col=4,
-                        value=time),
+                        value=tx_cobid),
                     ft.TextField(
-                        label="Node ID of the SDO Server",
+                        label="COB ID Rx SDO",
                         col=4,
-                        value=time),
+                        value=rx_cobid),
                     ft.IconButton(
                         icon=ft.icons.ADD,
                         icon_color="blue400",
@@ -74,18 +77,15 @@ class SdoCommunicationPanel(ft.ResponsiveRow):
                 ], ),
             )
 
-        self.container_cli = ft.Container()
         self.controls = [
             ft.ResponsiveRow(
                 [
                     ft.Container(
-                        ft.Text("SDO Client"),
-                        bgcolor=ft.colors.YELLOW,
+                        ft.Text("SDO Server"),
                         col=4,
                     ),
                     ft.Container(
-                        ft.Text("SDO Server"),
-                        bgcolor=ft.colors.GREEN,
+                        self.t_sdo_client,
                         col=8,
                     ),
                 ],
