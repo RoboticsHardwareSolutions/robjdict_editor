7bdd40e8738ca1155f15c839f71f4e44ed3a3ce0
PaulFirs <makloun@hotmail.com>
2025-02-26 23:26:14 +0300

    Updated main. Fix fast update the page

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
 import flet as ft
 from device_tab import DeviceTab
 import os
+import time
 
 os.environ["FLET_SECRET_KEY"] = os.urandom(12).hex()
 UPLOAD_DIR = 'upload'
+WINDOW_HEIGHT = 600
+WINDOW_WIDTH = 1000
+TAB_HEIGHT_OFFSET = 200
 
 def main(page: ft.Page):
     page.title = "Objdict editor"
     page.theme_mode = ft.ThemeMode.LIGHT
-    page.window.min_height = 600
-    page.window.height = 600
-    page.window.min_width = 1000
-    page.window.width = 1000
-    progressbar = ft.ProgressBar(width=page.width, value=0, visible=False)
+    page.window.min_height = WINDOW_HEIGHT
+    page.window.height = WINDOW_HEIGHT
+    page.window.min_width = WINDOW_WIDTH
+    page.window.width = WINDOW_WIDTH
 
     def page_resize(e):
         for tab in devices.tabs:
             if isinstance(tab, DeviceTab):
-                tab.life_communication.lv_consumer_hb.height = page.height - 200
-                tab.sdo_communication.lv_sdo_client.height = page.height - 200
-                tab.obj_dict.lv_obj.height = page.height - 200
-            page.update()
+                tab.life_communication.lv_consumer_hb.height = page.height - TAB_HEIGHT_OFFSET
+                tab.sdo_communication.lv_sdo_client.height = page.height - TAB_HEIGHT_OFFSET
+                tab.obj_dict.lv_obj.height = page.height - TAB_HEIGHT_OFFSET
+        page.update()
 
     page.on_resized = page_resize
 
@@ -31,44 +34,30 @@ def main(page: ft.Page):
     )
 
     def button_delete(e):
-        for device in devices.tabs:
-            if device.uid == e.control.parent.parent.uid:
-                devices.tabs.remove(device)
-                page.update()
+        devices.tabs = [device for device in devices.tabs if device.uid != e.control.parent.parent.uid]
+        page.update()
 
-    # File picker
     def file_picker_result(e: ft.FilePickerResultEvent):
         if e.files is None:
             return
-        files = []
-
-        for file in e.files:
-            files.append(
-                ft.FilePickerUploadFile(
-                    file.name,
-                    upload_url=page.get_upload_url(file.name, 600)
-                )
-            )
-
-        progressbar.visible = True
+        files = [
+            ft.FilePickerUploadFile(
+            file.name,
+            upload_url=page.get_upload_url(file.name, 600)
+            ) for file in e.files
+        ]
         file_picker_dialog.upload(files)
-        
-        for file in files:
-            while not os.path.exists(f"{UPLOAD_DIR}/{file.name}") or progressbar.visible == True:
-                pass
+
+    def file_picker_upload(e: ft.FilePickerUploadEvent):
+        if e.progress == 1:
             btn = ft.IconButton(
                 icon=ft.Icons.CLOSE,
                 on_click=button_delete,
             )
-            new_device = DeviceTab(f"{UPLOAD_DIR}/{file.name}", btn)
-            devices.tabs.append(new_device)
-            page.update()
-
-    def file_picker_upload(e: ft.FilePickerUploadEvent):
-        progressbar.value = e.progress
-        page.update()
-        if progressbar.value == 1:
-            progressbar.visible = False
+            device_tab = DeviceTab(f"{UPLOAD_DIR}/{e.file_name}", btn)
+            devices.tabs.append(device_tab)
+        
+        time.sleep(2) # flet's magic. app can to request a ui update even before completing the previous ui update request.
         page.update()
 
     file_picker_dialog = ft.FilePicker(
@@ -84,7 +73,6 @@ def main(page: ft.Page):
         except IndexError:
             return
 
-    # Create the top menu bar
     menubar = ft.AppBar(
         title=ft.Text("Objdict editor"),
         actions=[
@@ -96,10 +84,8 @@ def main(page: ft.Page):
     )
     page.add(
         menubar,
-        devices,
-        progressbar
+        devices
     )
     page.update()
-
 
 ft.app(target=main, view=ft.WEB_BROWSER, port=8551, upload_dir=UPLOAD_DIR)
