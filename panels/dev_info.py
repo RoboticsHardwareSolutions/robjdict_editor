351fbf22819bb82522cb419e5308d1fe69f3c199
Firsov Pavel Dmitrievich <p.d.firsov@gmail.com>
2025-04-02 18:25:45 +0300

    Optimized dev_info

--- panels/dev_info.py
+++ panels/dev_info.py
@@ -1,6 +1,3 @@
-import canopen
-import can
-from can import Message
 import flet as ft
 
 
@@ -12,17 +9,16 @@ class DevInfoPanel(ft.ResponsiveRow):
 
         # Define text fields with a helper method
         self.te_fields = {
-            "product_name": self._create_text_field("Product Name", od.object_dictionary.device_information.product_name),
+            "product_name": self._create_text_field("Product Name", od.object_dictionary.device_information.product_name,),
             "product_number": self._create_text_field("Product Number", od.object_dictionary.device_information.product_number),
             "vendor_name": self._create_text_field("Vendor Name", od.object_dictionary.device_information.vendor_name),
             "vendor_number": self._create_text_field("Vendor Number", od.object_dictionary.device_information.vendor_number),
             "node_id": self._create_text_field("Node ID", od.id),
-            "dev_type": self._create_text_field("Device Type", od.object_dictionary[0x1000].default),
+            "device_type": self._create_text_field("Device Type", od.object_dictionary[0x1000].default),
         }
-
         self.controls = [
             ft.Column(
-                [self.te_fields["node_id"], self.te_fields["dev_type"]],
+                [self.te_fields["node_id"], self.te_fields["device_type"]],
                 col={"sm": 6},
             ),
             ft.Column(
@@ -37,18 +33,15 @@ class DevInfoPanel(ft.ResponsiveRow):
         ]
 
     def _create_text_field(self, label, value):
+        def change(e):
+            field_name = e.control.label.lower().replace(" ", "_")
+            if field_name in self.te_fields:
+                if field_name == "node_id":
+                    self.od.id = e.control.value
+                elif field_name == "device_type":
+                    self.od.object_dictionary.get_variable(0x1000).default_raw = e.control.value
+                    self.od.object_dictionary.get_variable(0x1000).default = e.control.value
+                else:
+                    setattr(self.od.object_dictionary.device_information, field_name, e.control.value)
         """Helper method to create a TextField."""
-        return ft.TextField(label=label, value=value)
-
-    def update_od(self, od: canopen.objectdictionary):
-        """Update the object dictionary with the current text field values."""
-        od.object_dictionary.device_information.product_name = self.te_fields["product_name"].value
-        od.object_dictionary.device_information.product_number = self.te_fields["product_number"].value
-        od.object_dictionary.device_information.vendor_name = self.te_fields["vendor_name"].value
-        od.object_dictionary.device_information.vendor_number = self.te_fields["vendor_number"].value
-
-        od.id = self.te_fields["node_id"].value
-        od.object_dictionary.get_variable(0x1000).default_raw = self.te_fields["dev_type"].value
-        od.object_dictionary.get_variable(0x1000).default = self.te_fields["dev_type"].value
-
-        return od
+        return ft.TextField(label=label, value=value, on_change=change)
